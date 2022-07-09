0d5ddfc8afa294b698020b890fab55be66753e0e
lovelydaemon <slamy777@gmail.com>
2022-07-09 19:52:46 +0300

    a session is now created only once

--- task_02.py
+++ task_02.py
@@ -14,38 +14,37 @@ def find_all_pages() -> list:
     doc = html.fromstring(resp.text)
     return doc.xpath('//*[@class="plainlinks"]//a/@href')[2:]
 
-async def get_data(url):
+async def get_data(url, session):
     """Return the number of animals for a particular letter"""
     char = ''
     total = 0
-    async with aiohttp.ClientSession(headers=headers) as s:
-        while True:
-            async with s.get(url=url) as resp:
-                res = await resp.text()
+    while True:
+        async with session.get(url=url) as resp:
+            res = await resp.text()
 
-            doc = html.fromstring(res)
-            page = doc.xpath('//*[@id="mw-pages"]')[0]
-            category = page.xpath('.//*[@class="mw-category-group"][1]')[0]
-            next_char = category.xpath('.//h3/text()')[0]
-            names = len(category.xpath('.//ul/li'))
+        doc = html.fromstring(res)
+        page = doc.xpath('//*[@id="mw-pages"]')[0]
+        category = page.xpath('.//*[@class="mw-category-group"][1]')[0]
+        next_char = category.xpath('.//h3/text()')[0]
+        names = len(category.xpath('.//ul/li'))
 
-            if not char: char = next_char
-            if next_char != char: break
+        if not char: char = next_char
+        if next_char != char: break
 
-            total += names
+        total += names
 
-            try:
-                next_page = page.xpath('.//a[contains(text(), "Следующая страница")]/@href')[0]
-                url = f'{root}{next_page}'
-            except:
-                break
+        try:
+            url = page.xpath('.//a[contains(text(), "Следующая страница")]/@href')[0]
+        except:
+            break
     return char, total
 
 
 async def main():
     """Return the dictionary with count of animals"""
-    tasks = [asyncio.create_task(get_data(page)) for page in find_all_pages()]
-    res = dict(await asyncio.gather(*tasks))
+    async with aiohttp.ClientSession(base_url=root, headers=headers) as session:
+        tasks = [asyncio.create_task(get_data(url.removeprefix(root), session)) for url in find_all_pages()]
+        res = dict(await asyncio.gather(*tasks))
 
     for k,v in res.items():
         print(k, v)
