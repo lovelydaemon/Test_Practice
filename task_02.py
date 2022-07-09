aea975e4f0082389bae6088c57582bec37cbebf2
lovelydaemon <slamy777@gmail.com>
2022-07-09 22:31:09 +0300

    minor fixes

--- task_02.py
+++ task_02.py
@@ -3,21 +3,28 @@ import requests
 from lxml import html
 import aiohttp
 
-root = 'https://ru.wikipedia.org'
-headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
+ROOT = 'https://ru.wikipedia.org'
+HEADERS = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
+CONNECTION_LIMIT = 50
 
 def find_all_pages() -> list:
     """Return pages А-Я and A-Z"""
-    entry_point = f'{root}/wiki/Категория:Животные_по_алфавиту'
-    resp = requests.get(url=entry_point, headers=headers)
-    resp.raise_for_status()
+    entry_point = f'{ROOT}/wiki/Категория:Животные_по_алфавиту'
+    resp = requests.get(url=entry_point, headers=HEADERS)
+    try:
+        resp.raise_for_status()
+    except Exception as e:
+        print(e)
+        exit()
+
     doc = html.fromstring(resp.text)
     return doc.xpath('//*[@class="plainlinks"]//a/@href')[2:]
 
-async def get_data(url, session):
+async def get_data(url, session) -> tuple[str, int]:
     """Return the number of animals for a particular letter"""
-    char = ''
-    total = 0
+    char:str = ''
+    total:int = 0
+
     while True:
         async with session.get(url=url) as resp:
             res = await resp.text()
@@ -42,8 +49,12 @@ async def get_data(url, session):
 
 async def main():
     """Return the dictionary with count of animals"""
-    async with aiohttp.ClientSession(base_url=root, headers=headers) as session:
-        tasks = [asyncio.create_task(get_data(url.removeprefix(root), session)) for url in find_all_pages()]
+    async with aiohttp.ClientSession(
+            base_url=ROOT,
+            headers=HEADERS,
+            connector=aiohttp.TCPConnector(limit=CONNECTION_LIMIT)
+        ) as session:
+        tasks = [asyncio.create_task(get_data(url.removeprefix(ROOT), session)) for url in find_all_pages()]
         res = dict(await asyncio.gather(*tasks))
 
     for k,v in res.items():
