3abebe7d82bbc6633570ae7c9b08086f90a7614b
lovelydaemon <slamy777@gmail.com>
2022-07-09 17:14:28 +0300

    async method is 10x faster, but need some fixs

--- task_02.py
+++ task_02.py
@@ -1,79 +1,56 @@
+import asyncio
 import requests
-from bs4 import BeautifulSoup
+from lxml import html
+import aiohttp
 
+root = 'https://ru.wikipedia.org'
+headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
 
-headers = {
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
-}
-
-def find_next_page(current_page) -> str:
-    """Return the next page URN"""
-    return current_page.find_all('a', text='Следующая страница')[-1].get('href')
-
-def find_all_pages(url, headers) -> list:
+def find_all_pages() -> list:
     """Return pages А-Я and A-Z"""
-    pages = []
-    response = requests.get(url=url, headers=headers)
-    try:
-        response.raise_for_status()
-    except requests.HTTPError as e:
-        print(e)
-        return pages
+    entry_point = f'{root}/wiki/Категория:Животные_по_алфавиту'
+    resp = requests.get(url=entry_point, headers=headers)
+    resp.raise_for_status()
+    doc = html.fromstring(resp.text)
+    return doc.xpath('//*[@class="plainlinks"]//a/@href')[2:]
 
-    soup = BeautifulSoup(response.text, 'lxml')
-    links = soup.find('table', class_='plainlinks').find_all('a')
-    for item in links[2:]:
-        pages.append(item.get('href'))
-    return pages
-
-def get_data(url_base, url, headers):
-    """Return first character of animals and their count"""
+async def get_data(url):
+    """Return the number of animals for a particular letter"""
     char = ''
     total = 0
-    while True:
-        response = requests.get(url=url, headers=headers)
-        try:
-            response.raise_for_status()
-        except requests.HTTPError as e:
-            print(e)
-            break
+    async with aiohttp.ClientSession(headers=headers) as s:
+        while True:
+            async with s.get(url=url) as resp:
+                res = await resp.text()
 
-        soup = BeautifulSoup(response.text, 'lxml')
-        page = soup.find(id='mw-pages')
-        names_list = page.find('div', class_='mw-category-group')
-        next_char = names_list.h3.string
+            doc = html.fromstring(res)
+            page = doc.xpath('//*[@id="mw-pages"]')[0]
+            category = page.xpath('.//*[@class="mw-category-group"][1]')[0]
+            next_char = category.xpath('.//h3/text()')[0]
+            names = len(category.xpath('.//ul/li'))
 
-        if not char: char = next_char
-        if next_char != char: break
+            if not char: char = next_char
+            if next_char != char: break
 
-        total += len(names_list.find_all('li'))
+            total += names
 
-        try:
-            url = f'{url_base}{find_next_page(page)}'
-        except:
-            break
+            try:
+                next_page = page.xpath('.//a[contains(text(), "Следующая страница")]/@href')[0]
+                url = f'{root}{next_page}'
+            except:
+                break
     return char, total
 
 
-def fetch(headers:dict|None) -> dict:
+async def main():
     """Return the dictionary with count of animals"""
-    data = {}
-    URL_BASE = 'https://ru.wikipedia.org'
-    START_POINT = f'{URL_BASE}/wiki/Категория:Животные_по_алфавиту'
+    tasks = [asyncio.create_task(get_data(page)) for page in find_all_pages()]
+    res = dict(await asyncio.gather(*tasks))
 
-    pages_for_searching = find_all_pages(START_POINT, headers)
-
-    for page in pages_for_searching:
-        ch, count = get_data(URL_BASE, page, headers)
-        data[ch] = count
-
-    return data
+    for k,v in res.items():
+        print(k, v)
+    print(sum(res.values()))
 
 
 if __name__ == '__main__':
-    data = fetch(headers)
-    for k, v in data.items():
-        print(k, v)
-    print(sum(data.values()))
-# Result in 50s - 2m
+    asyncio.run(main())
